f522382d99ca93b60c3f3fe4bdcfadd9d9b212a7
Ben Hodge <benjamin.andrew.hodge@gmail.com>
2019-07-22 15:23:29 -0500

    finsihed security course

---
+++ node_security/base/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import routes from "./src/routes/crmRoutes";
+import helmet from "helmet";
+import RateLimit from "express-rate-limit";
+
+const app = express();
+const PORT = 3000;
+// setup helmet
+app.use(helmet());
+
+// setup rate limit
+const limiter = new RateLimit({
+    windowMs: 15 * 60 * 1000, // stay alive for 15 mins
+    max: 100, //limit request number
+    delayMs: 0 // if you reach the limit, delay 0 ms
+});
+// mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/CRMdb");
+
+// bodyparser setup
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+routes(app);
+
+// serving static files
+app.use(express.static("public"));
+
+app.get("/", (req, res) =>
+    res.send(`Node and express server is running on port ${PORT}`)
+);
+
+app.listen(PORT, () => console.log(`your server is running on port ${PORT}`));
