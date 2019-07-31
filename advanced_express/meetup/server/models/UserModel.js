3a2f015cce5fb3d6e5971b776477ab533378f954
benhodge <benjamin.andrew.hodge@gmail.com>
2019-07-31 13:17:24 -0500

    rewatching course

--- advanced_express/meetup/server/models/UserModel.js
+++ advanced_express/meetup/server/models/UserModel.js
@@ -4,52 +4,52 @@ const bcrypt = require("bcrypt");
 const SALT_ROUNDS = 12;
 
 const UserSchema = mongoose.Schema(
-    {
-        username: {
-            type: String,
-            required: true,
-            trim: true,
-            index: { unique: true },
-            minlength: 3
-        },
-        email: {
-            type: String,
-            required: true,
-            trim: true,
-            lowercase: true,
-            index: { unique: true },
-            validate: {
-                validator: emailValidator.validate,
-                message: props => `${props.value} is not valid email address`
-            }
-        },
-        password: {
-            type: String,
-            required: true,
-            trim: true,
-            index: { unique: true },
-            minlength: 8
-        }
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      index: { unique: true },
+      minlength: 3
     },
-    {
-        timestamps: true
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      index: { unique: true },
+      validate: {
+        validator: emailValidator.validate,
+        message: props => `${props.value} is not valid email address`
+      }
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      index: { unique: true },
+      minlength: 8
     }
+  },
+  {
+    timestamps: true
+  }
 );
 
 UserSchema.pre("save", async function presave(next) {
-    const user = this;
-    if (!user.isModified("password")) return next();
-    try {
-        const hash = await bcrypt.hash(user.password, SALT_ROUNDS);
-        user.password = hash;
-        return next();
-    } catch (err) {
-        return next(err);
-    }
+  const user = this;
+  if (!user.isModified("password")) return next();
+  try {
+    const hash = await bcrypt.hash(user.password, SALT_ROUNDS);
+    user.password = hash;
+    return next();
+  } catch (err) {
+    return next(err);
+  }
 });
 
 UserSchema.methods.comparePassword = async function comparePassword(candidate) {
-    return bcrypt.compare(candidate, this.password);
+  return bcrypt.compare(candidate, this.password);
 };
 
 module.exports = mongoose.model("User", UserSchema);
